44363ccd228d9eb80e655c4d8a2dc3355f5de25c
neosome_college <daniildaresin@gmail.com>
2025-03-31 10:31:31 +0300

    update hw_13_1

---
+++ data/transactions_csv.py
@@ -0,0 +1,3 @@
+import os
+
+TRANSACTIONS_CSV_PATH = os.path.join(os.path.dirname(__file__), 'transactions.csv')
---
+++ data/transactions_excel.py
@@ -0,0 +1,3 @@
+import os
+
+TRANSACTIONS_EXCEL_PATH = os.path.join(os.path.dirname(__file__), 'transactions_excel.xlsx')
