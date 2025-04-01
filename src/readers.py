120a2c2f93f6b2db2d976c6df4a07412c098d1e2
neosome <167439712+neosomee@users.noreply.github.com>
2025-04-01 19:36:47 +0300

    Update readers.py

--- src/readers.py
+++ src/readers.py
@@ -1,29 +1,23 @@
 import pandas as pd
 
-def read_csv_transactions(file_path):
-    """
-    Чтение CSV-файла.
-    """
-    try:
-        df = pd.read_csv(file_path)
-        return df.to_dict('records')
-    except Exception as e:
-        print(f"Ошибка чтения CSV: {str(e)}")
-        return []
 
-def read_excel_transactions(file_path):
-    """
-    Чтение Excel-файла.
-    """
+def CSV_file_read(file_path):
+    """Принимает на вход путь до файла CSV и возвращает список словарей со всеми транзакциями"""
+    try:
+        df = pd.read_csv(file_path, delimiter=";")
+        fixed_df = df.dropna(how="any")
+        list_of_dicts = fixed_df.to_dict(orient="records")
+        return list_of_dicts
+    except FileNotFoundError:
+        return "Файл не найден"
+
+
+def XLSX_file_read(file_path):
+    """Принимает на вход путь до файла XLSX и возвращает список словарей со всеми транзакциями"""
     try:
         df = pd.read_excel(file_path)
-        return df.to_dict('records')
-    except Exception as e:
-        print(f"Ошибка чтения Excel: {str(e)}")
-        return []
-
-csv_file_path = 'C:\\Users\\Dareshin.D\\Downloads\\transactions.csv'
-excel_file_path = 'C:\\Users\\Dareshin.D\\Downloads\\transactions_excel.xlsx'
-
-csv_data = read_csv_transactions(csv_file_path)
-excel_data = read_excel_transactions(excel_file_path)
+        fixed_df = df.dropna(how="any")
+        list_of_dicts = fixed_df.to_dict(orient="records")
+        return list_of_dicts
+    except FileNotFoundError:
+        return "Файл не найден"
