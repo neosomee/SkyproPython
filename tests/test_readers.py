409c474a1fd35ca11c03574e1f5e5d7c04171dee
neosome <167439712+neosomee@users.noreply.github.com>
2025-04-01 19:39:04 +0300

    Update test_readers.py

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -1,30 +1,39 @@
-import pandas as pd
-import pytest
 from unittest.mock import patch
-from src.reader import read_csv_transactions, read_excel_transactions
 
-MOCK_CSV_DATA = pd.DataFrame({
-    'id': [1, 2, 3],
-    'state': ['EXECUTED', 'CANCELED', 'EXECUTED'],
-    'to': ['Visa 1234', 'MasterCard 5678', 'Visa 9012'],
-    'description': ['Перевод с карты на карту', 'Открытие вклада', 'Перевод организации']
-})
+import pandas as pd
 
-MOCK_EXCEL_DATA = pd.DataFrame({
-    'id': [10, 20, 30],
-    'state': ['EXECUTED', 'CANCELED', 'EXECUTED'],
-    'to': ['Discover 0987', 'American Express 6543', 'Visa 2109'],
-    'description': ['Перевод с карты на карту', 'Открытие вклада', 'Перевод организации']
-})
+from src import files_reader
 
-def test_read_csv_success(mocker):
-    """Проверяет успешное чтение CSV-файла."""
-    mocker.patch('pandas.read_csv', return_value=MOCK_CSV_DATA)
-    result = read_csv_transactions('example.csv')
-    pd.testing.assert_frame_equal(result, MOCK_CSV_DATA)
 
-def test_read_excel_success(mocker):
-    """Проверяет успешное чтение Excel-файла."""
-    mocker.patch('pandas.read_excel', return_value=MOCK_EXCEL_DATA)
-    result = read_excel_transactions('example.xlsx')
-    pd.testing.assert_frame_equal(result, MOCK_EXCEL_DATA)
+@patch("pandas.read_csv")
+def test_CSV_file_read_patch(mock_get):
+    data = {"id": ["1", "2", "3"], "amount": ["452", "3453", "1420"], "currency_code": ["USD", "RUB", "USD"]}
+    df = pd.DataFrame(data)
+    mock_get.return_value = df
+    assert files_reader.CSV_file_read("..\\tests\\data_test\\transactions.csv") == [
+        {"id": "1", "amount": "452", "currency_code": "USD"},
+        {"id": "2", "amount": "3453", "currency_code": "RUB"},
+        {"id": "3", "amount": "1420", "currency_code": "USD"},
+    ]
+    mock_get.assert_called_once_with("..\\\\data\\transactions.csv", delimiter=";")
+
+
+def test_CSV_file_read():
+    assert files_reader.CSV_file_read("psorjtgrsij") == "Файл не найден"
+
+
+@patch("pandas.read_excel")
+def test_XLSX_file_read_patch(mock_get):
+    data = {"id": ["1", "2", "3"], "amount": ["452", "3453", "1420"], "currency_code": ["USD", "RUB", "USD"]}
+    df = pd.DataFrame(data)
+    mock_get.return_value = df
+    assert files_reader.XLSX_file_read("..\\tests\\data_test\\transactions_excel.xlsx") == [
+        {"id": "1", "amount": "452", "currency_code": "USD"},
+        {"id": "2", "amount": "3453", "currency_code": "RUB"},
+        {"id": "3", "amount": "1420", "currency_code": "USD"},
+    ]
+    mock_get.assert_called_once_with("..\\\\data\\transactions_excel.xlsx")
+
+
+def test_XLSX_file_read():
+    assert files_reader.XLSX_file_read("psorjtgrsij") == "Файл не найден"
