44abcc83518ef906740150390541fe5ae7e5de26
neosome_college <daniildaresin@gmail.com>
2025-04-01 09:52:40 +0300

    update hw_13_1

--- data/transactions_excel.py
+++
@@ -1,3 +0,0 @@
-import os
-
-TRANSACTIONS_EXCEL_PATH = os.path.join(os.path.dirname(__file__), 'transactions_excel.xlsx')
--- src/reader.py
+++ src/reader.py
@@ -1,21 +1,29 @@
 import pandas as pd
-from SkyproPython.data.transactions_csv import TRANSACTIONS_CSV_PATH
-from SkyproPython.data.transactions_excel import TRANSACTIONS_EXCEL_PATH
 
-def read_csv_transactions():
-    """Чтение CSV-файла"""
+def read_csv_transactions(file_path):
+    """
+    Чтение CSV-файла.
+    """
     try:
-        df = pd.read_csv(TRANSACTIONS_CSV_PATH)
+        df = pd.read_csv(file_path)
         return df.to_dict('records')
     except Exception as e:
         print(f"Ошибка чтения CSV: {str(e)}")
         return []
 
-def read_excel_transactions():
-    """Чтение Excel-файла с обработкой ошибок"""
+def read_excel_transactions(file_path):
+    """
+    Чтение Excel-файла.
+    """
     try:
-        df = pd.read_excel(TRANSACTIONS_EXCEL_PATH)
+        df = pd.read_excel(file_path)
         return df.to_dict('records')
     except Exception as e:
         print(f"Ошибка чтения Excel: {str(e)}")
         return []
+
+csv_file_path = 'C:\\Users\\Dareshin.D\\Downloads\\transactions.csv'
+excel_file_path = 'C:\\Users\\Dareshin.D\\Downloads\\transactions_excel.xlsx'
+
+csv_data = read_csv_transactions(csv_file_path)
+excel_data = read_excel_transactions(excel_file_path)
